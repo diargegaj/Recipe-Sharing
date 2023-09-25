04f95edc498f5d6bb6054f896bd0f2334fab462d
Diar Gegaj <gegajdiar1@gmail.com>
2023-09-25 20:15:13 +0200

    Show recipes as grid layout on profile. Instead of button that navigate to your recipes. Show recipe details when a recipe on profile is clicked.

--- app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
+++ app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
@@ -2,16 +2,19 @@ package com.diargegaj.recipesharing.presentation.screens.home.profile
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,8 +31,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.diargegaj.recipesharing.R
+import com.diargegaj.recipesharing.domain.models.recipe.RecipeModel
 import com.diargegaj.recipesharing.presentation.navigation.RecipeNavigationActions
 import com.diargegaj.recipesharing.presentation.utils.ImagePicker
+import com.diargegaj.recipesharing.presentation.utils.LoadImage
 import com.diargegaj.recipesharing.presentation.viewModel.home.profile.UserProfileViewModel
 
 @Composable
@@ -64,9 +69,12 @@ fun ProfileScreen(
                 modifier = Modifier
                     .size(30.dp)
                     .align(Alignment.BottomEnd),
-                image  = null,
+                image = null,
                 editView = {
-                    Image(painter = painterResource(id = R.drawable.ic_edit), contentDescription = "Edit icon")
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_edit),
+                        contentDescription = "Edit icon"
+                    )
                 },
                 onImagePicked = { pickedImage ->
                     viewModel.updateProfileImage(pickedImage)
@@ -77,7 +85,7 @@ fun ProfileScreen(
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "${userState.name} ${userState.lastName}",
+            text = "${userState.getUserFullName()}",
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
@@ -85,17 +93,38 @@ fun ProfileScreen(
         Spacer(modifier = Modifier.height(8.dp))
 
         Text(text = userState.email, textAlign = TextAlign.Center)
-    }
-    Row(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = {
-            recipeNavigationActions.navigateToRecipes(userState.userUUID)
-        }) {
-            Text(text = stringResource(id = R.string.my_recipes))
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = stringResource(id = R.string.my_recipes),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(4.dp)
+        ) {
+            itemsIndexed(userState.userRecipes) { _, recipe ->
+                RecipeThumbnail(recipe = recipe, modifier = Modifier
+                    .size(100.dp)
+                    .padding(4.dp)
+                    .clickable {
+                        recipeNavigationActions.navigateToRecipeDetails(recipe.recipeId)
+                    })
+            }
         }
     }
+}
+
+@Composable
+fun RecipeThumbnail(recipe: RecipeModel, modifier: Modifier = Modifier) {
+    LoadImage(
+        imageUrl = recipe.imageUrl,
+        modifier = modifier
+    )
+
 }
