bdf2bcc8975bef2d79717ad39ab01a432e97f884
Diar Gegaj <gegajdiar1@gmail.com>
2023-10-01 20:11:03 +0200

    Add option to add recipe ingredient.

--- app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/recipes/editRecipe/EditRecipeScreen.kt
+++ app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/recipes/editRecipe/EditRecipeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
@@ -148,6 +149,14 @@ fun EditRecipeScreen(
                     }
 
                     Spacer(modifier = Modifier.height(16.dp))
+                }
+
+                item {
+                    IconButton(onClick = {
+                        viewModel.addIngredient()
+                    }) {
+                        Icon(Icons.Default.Add, contentDescription = "Add ingredient")
+                    }
                 }
 
                 item {
