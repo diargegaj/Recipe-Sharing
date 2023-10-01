16ad1dc3563d1a5648c016f4f5e06553f5b64d26
Diar Gegaj <gegajdiar1@gmail.com>
2023-10-01 12:59:10 +0200

    Remove feature to change profile photo from user profile screen.

--- app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
+++ app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
@@ -1,7 +1,6 @@
 package com.diargegaj.recipesharing.presentation.screens.home.profile
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,17 +22,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
 import com.diargegaj.recipesharing.R
 import com.diargegaj.recipesharing.domain.models.recipe.RecipeModel
 import com.diargegaj.recipesharing.presentation.navigation.RecipeNavigationActions
-import com.diargegaj.recipesharing.presentation.utils.ImagePicker
 import com.diargegaj.recipesharing.presentation.utils.LoadImage
 import com.diargegaj.recipesharing.presentation.viewModel.home.profile.UserProfileViewModel
 
@@ -50,42 +46,23 @@ fun ProfileScreen(
     }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
-            modifier = Modifier.size(120.dp),
-            contentAlignment = Alignment.BottomEnd
+            modifier = Modifier.size(120.dp), contentAlignment = Alignment.BottomEnd
         ) {
-            AsyncImage(
-                model = userState.profilePhotoUrl,
-                contentDescription = "User Profile Picture",
+            LoadImage(
+                imageUrl = userState.profilePhotoUrl,
                 modifier = Modifier
                     .fillMaxSize()
                     .clip(CircleShape)
-            )
-            ImagePicker(
-                modifier = Modifier
-                    .size(30.dp)
-                    .align(Alignment.BottomEnd),
-                image = null,
-                editView = {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_edit),
-                        contentDescription = "Edit icon"
-                    )
-                },
-                onImagePicked = { pickedImage ->
-                    viewModel.updateProfileImage(pickedImage)
-                }
             )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "${userState.getUserFullName()}",
+            text = userState.getUserFullName(),
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
@@ -109,12 +86,14 @@ fun ProfileScreen(
             contentPadding = PaddingValues(4.dp)
         ) {
             itemsIndexed(userState.userRecipes) { _, recipe ->
-                RecipeThumbnail(recipe = recipe, modifier = Modifier
-                    .size(100.dp)
-                    .padding(4.dp)
-                    .clickable {
-                        recipeNavigationActions.navigateToRecipeDetails(recipe.recipeId)
-                    })
+                RecipeThumbnail(
+                    recipe = recipe,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(4.dp)
+                        .clickable {
+                            recipeNavigationActions.navigateToRecipeDetails(recipe.recipeId)
+                        })
             }
         }
     }
@@ -123,8 +102,7 @@ fun ProfileScreen(
 @Composable
 fun RecipeThumbnail(recipe: RecipeModel, modifier: Modifier = Modifier) {
     LoadImage(
-        imageUrl = recipe.imageUrl,
-        modifier = modifier
+        imageUrl = recipe.imageUrl, modifier = modifier
     )
 
 }
