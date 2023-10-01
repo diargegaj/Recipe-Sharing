5707a85e31eab088fc6d4483343a25e46a80bd01
Diar Gegaj <gegajdiar1@gmail.com>
2023-10-01 13:10:58 +0200

    re-design recipe list.

--- app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/recipes/RecipesList.kt
+++ app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/recipes/RecipesList.kt
@@ -1,12 +1,15 @@
 package com.diargegaj.recipesharing.presentation.screens.home.recipes
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
@@ -16,11 +19,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.diargegaj.recipesharing.domain.models.recipe.RecipeModel
 import com.diargegaj.recipesharing.domain.models.UserModel
+import com.diargegaj.recipesharing.domain.models.recipe.RecipeModel
 import com.diargegaj.recipesharing.presentation.navigation.RecipeNavigationActions
 import com.diargegaj.recipesharing.presentation.utils.LoadImage
 
@@ -55,7 +60,15 @@ fun RecipePost(
 ) {
     Row(
         modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
             .clickable(onClick = { navigateToRecipe(recipeModel.recipeId) })
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .shadow(4.dp, RoundedCornerShape(8.dp))
+            .padding(16.dp)
     ) {
         PostImage(
             imageUrl = recipeModel.imageUrl,
