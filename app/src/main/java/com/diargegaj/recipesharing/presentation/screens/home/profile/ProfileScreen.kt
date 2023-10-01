bec5d58557b0d64a83ba2e0ef7df271b51c72383
Diar Gegaj <gegajdiar1@gmail.com>
2023-10-01 13:11:23 +0200

    re-design recipe thumbnail at profile screen.

--- app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
+++ app/src/main/java/com/diargegaj/recipesharing/presentation/screens/home/profile/ProfileScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -21,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,7 +51,11 @@ fun ProfileScreen(
         modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
-            modifier = Modifier.size(120.dp), contentAlignment = Alignment.BottomEnd
+            modifier = Modifier
+                .size(120.dp)
+                .shadow(4.dp, CircleShape) // Shadow for depth
+                .clip(CircleShape),
+            contentAlignment = Alignment.BottomEnd
         ) {
             LoadImage(
                 imageUrl = userState.profilePhotoUrl,
@@ -103,6 +109,8 @@ fun ProfileScreen(
 fun RecipeThumbnail(recipe: RecipeModel, modifier: Modifier = Modifier) {
     LoadImage(
         imageUrl = recipe.imageUrl, modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .shadow(2.dp, RoundedCornerShape(8.dp))
     )
 
 }
